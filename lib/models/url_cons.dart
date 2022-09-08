2c22a7cc4fd9e03a2eb46b006c06a4e768417884
Dayumi Annisa <annisadayumipulungan@gmail.com>
2022-09-08 12:04:28 +0700

    add new dev url

--- lib/models/url_cons.dart
+++ lib/models/url_cons.dart
@@ -1,30 +1,58 @@
 class UrlCons {
-  static String mainProdUrl = 'https://devapi-mobile.pgn.co.id/v2/';
+  // static String mainProdUrl = 'https://devapi-mobile.pgn.co.id/v2/';
+  // // static String mainProdUrl = 'https://api-mobile.pgn.co.id/v2/';
+  // // static String prodRelyonUrl = 'https://relyon-api.pgn.co.id/';
+  // static String prodRelyonUrl = 'https://dev-relyon-api.pgn.co.id/';
+  // static String mainDevUrl = 'https://devapi-mobile.pgn.co.id/v2/';
+
+  // // static String getTokenBarrer =
+  // //     'https://api-mobile.pgn.co.id/v2/authentication';
+  // //dev
+  // static String getSpbgArea = 'https://devapi-mobile.pgn.co.id/v2/spbg';
+  // static String getCustProfileCust =
+  //     'https://devapi-mobile.pgn.co.id/v2/customers/me';
+  // static String getCustInvoiceCust =
+  //     'https://devapi-mobile.pgn.co.id/v2/customers/me/invoices';
+  // static String getGasUsageSumCust =
+  //     'https://devapi-mobile.pgn.co.id/v2/customers/me/gas-usages-summary';
+  // static String getGasUsageMonthlyChartCust =
+  //     'https://devapi-mobile.pgn.co.id/v2/customers/me/gas-usages/monthly-chart';
+  // static String getGasUsageDailyChartCust =
+  //     'https://devapi-mobile.pgn.co.id/v2/customers/me/gas-usages/daily-chart';
+  // static String getDataCustomerMng =
+  //     'https://devapi-mobile.pgn.co.id/v2/customers/';
+  // static String getUsageContractChart =
+  //     'https://devapi-mobile.pgn.co.id/v2/summary/usage-contract-chart/';
+  // static String getInvoiceResidentialChart =
+  //     "https://devapi-mobile.pgn.co.id/v2/residence-invoices/";
+  // static String getInvoiceRavamp =
+  //     "https://sandbox.pgn.co.id/api-mobile/PGNCRMCustPaymentRS";
+
+//DEV NEW MIGRASI TAHAP 2
+  static String mainProdUrl = 'https://10.129.2.63/v2/';
   // static String mainProdUrl = 'https://api-mobile.pgn.co.id/v2/';
   // static String prodRelyonUrl = 'https://relyon-api.pgn.co.id/';
   static String prodRelyonUrl = 'https://dev-relyon-api.pgn.co.id/';
-  static String mainDevUrl = 'https://devapi-mobile.pgn.co.id/v2/';
+  static String mainDevUrl = 'https://10.129.2.63/v2/';
 
   // static String getTokenBarrer =
   //     'https://api-mobile.pgn.co.id/v2/authentication';
   //dev
-  static String getSpbgArea = 'https://devapi-mobile.pgn.co.id/v2/spbg';
-  static String getCustProfileCust =
-      'https://devapi-mobile.pgn.co.id/v2/customers/me';
+  static String getSpbgArea = 'https://10.129.2.63/v2/spbg';
+  static String getCustProfileCust = 'https://10.129.2.63/v2/customers/me';
   static String getCustInvoiceCust =
-      'https://devapi-mobile.pgn.co.id/v2/customers/me/invoices';
+      'https://10.129.2.63/v2/customers/me/invoices';
   static String getGasUsageSumCust =
-      'https://devapi-mobile.pgn.co.id/v2/customers/me/gas-usages-summary';
+      'https://10.129.2.63/v2/me/gas-usages-summary';
   static String getGasUsageMonthlyChartCust =
-      'https://devapi-mobile.pgn.co.id/v2/customers/me/gas-usages/monthly-chart';
+      'https://10.129.2.63/v2/me/gas-usages/monthly-chart';
   static String getGasUsageDailyChartCust =
-      'https://devapi-mobile.pgn.co.id/v2/customers/me/gas-usages/daily-chart';
-  static String getDataCustomerMng =
-      'https://devapi-mobile.pgn.co.id/v2/customers/';
+      'https://10.129.2.63/v2/customers/me/gas-usages/daily-chart';
+  static String getDataCustomerMng = 'https://10.129.2.63/v2/customers/';
   static String getUsageContractChart =
-      'https://devapi-mobile.pgn.co.id/v2/summary/usage-contract-chart/';
+      'https://10.129.2.63/v2/summary/usage-contract-chart/';
   static String getInvoiceResidentialChart =
-      "https://devapi-mobile.pgn.co.id/v2/residence-invoices/";
+      "https://10.129.2.63/v2/residence-invoices/";
   static String getInvoiceRavamp =
       "https://sandbox.pgn.co.id/api-mobile/PGNCRMCustPaymentRS";
 
